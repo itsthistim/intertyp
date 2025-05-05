359e48cd48fd8afd917d9cf408fae05479b614fa
itsthistim <timhofmann01@gmail.com>
2025-05-05 22:25:29 +0200

    fix: make @ paths work

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,8 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import icon from "astro-icon";
-
 import node from "@astrojs/node";
+import path from "path";
 
 // https://astro.build/config
 export default defineConfig({
@@ -14,5 +13,12 @@ export default defineConfig({
 	integrations: [icon()],
 	devToolbar: {
 		enabled: false
+	},
+	vite: {
+		resolve: {
+			alias: {
+				"@": path.resolve("./src")
+			}
+		}
 	}
 });
