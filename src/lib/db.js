d1a4362caf781aaaf013ed7404c0eec8af59b62a
itsthistim <timhofmann01@gmail.com>
2025-05-10 20:23:28 +0200

    feat(api): enhance project retrieval with detailed query and error handling feat(db): switch to environment variables for database configuration

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,12 +1,19 @@
 import mysql from "mysql2/promise";
+import { getSecret } from "astro:env/server";
 
-const pool = mysql.createPool({
-	host: "127.0.0.1",
-	user: "root",
-	password: "",
-	database: "intertypcms",
-	waitForConnections: true,
-	connectionLimit: 10
-});
+let pool;
+try {
+	pool = mysql.createPool({
+		host: getSecret("DB_HOST"),
+		user: getSecret("DB_USER"),
+		password: getSecret("DB_PASSWORD"),
+		database: getSecret("DB_NAME"),
+		waitForConnections: true,
+		connectionLimit: 50
+	});
+} catch (error) {
+	console.error("Failed to create database pool:", error);
+	process.exit(1);
+}
 
 export default pool;
--- src/pages/api/projects.js
+++ src/pages/api/projects.js
@@ -1,6 +1,20 @@
 import db from "@/lib/db.js";
 
 export async function GET() {
-  const [rows] = await db.query("SELECT * FROM project");
-  return new Response(JSON.stringify(rows));
+	try {
+		const [rows] = await db.query(`SELECT p.project_id, p.title "project_title", p.description "project_description", p.project_date, i.url "cover_url", i.alt "cover_alt", l.slug "link_slug", l.name "link_name", g.title "gallery_title"
+                                     FROM project p
+                                     JOIN link l ON l.link_id = p.link_id
+                                     LEFT JOIN image i ON i.image_id = p.cover_image_id
+                                     LEFT JOIN gallery g ON g.gallery_id = p.gallery_id`);
+		return new Response(JSON.stringify(rows), {
+			status: 200,
+			headers: { "Content-Type": "application/json" }
+		});
+	} catch (err) {
+		return new Response(JSON.stringify({ error: "Database " + err }), {
+			status: 500,
+			headers: { "Content-Type": "application/json" }
+		});
+	}
 }
