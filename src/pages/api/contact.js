58f0554a287737dd0b657cdc3f78b460849cfe6a
itsthistim <timhofmann01@gmail.com>
2025-06-10 21:18:49 +0200

    feat(contact): integrate reCAPTCHA validation in contact form submission, closes #19

--- src/pages/api/contact.js
+++ src/pages/api/contact.js
@@ -8,25 +8,53 @@ export async function POST({ request }) {
 		const email = data.get("email");
 		const subject = data.get("subject");
 		const message = data.get("message");
+		const recaptchaToken = data.get("g-recaptcha-response");
 
-		const transporter = nodemailer.createTransport({
-			service: "gmail",
-			auth: {
-				user: getSecret("GMAIL_USER"),
-				pass: getSecret("GMAIL_APP_PASS")
-			}
-		});
+		if (validRecaptchaToken(recaptchaToken)) {
+			const transporter = nodemailer.createTransport({
+				service: "gmail",
+				auth: {
+					user: getSecret("GMAIL_USER"),
+					pass: getSecret("GMAIL_APP_PASS")
+				}
+			});
 
-		const info = await transporter.sendMail({
-			from: `"${name}" <${email}>`,
-			to: getSecret("GMAIL_USER"),
-			subject: subject,
-			html: `<p>Nachricht von <strong>${name}</strong> (<a href="mailto:${email}">${email}</a>):</p><br /><p>${message}</p>`
-		});
+			await transporter.sendMail({
+				from: `"${name}" <${email}>`,
+				to: getSecret("GMAIL_USER"),
+				subject: subject,
+				html: `<p>Nachricht von <strong>${name}</strong> (<a href="mailto:${email}">${email}</a>):</p>
+						<br />
+						<p>${message}</p>`
+			});
+		}
 
 		return new Response(JSON.stringify({ success: true, message: "Nachricht erfolgreich gesendet." }), { status: 200, headers: { "Content-Type": "application/json" } });
 	} catch (e) {
 		console.error(e);
 		return new Response(JSON.stringify({ success: false, message: "Fehler beim Senden der Nachricht." }), { status: 500, headers: { "Content-Type": "application/json" } });
 	}
+}
+
+async function validRecaptchaToken() {
+	if (!recaptchaToken) {
+		return new Response(JSON.stringify({ success: false, message: "reCAPTCHA fehlgeschlagen. Versuchen Sie es später erneut oder kontaktieren Sie stempel@intertyp.at" }), {
+			status: 400,
+			headers: { "Content-Type": "application/json" }
+		});
+	}
+
+	const recaptchaResponse = await fetch(`https://www.google.com/recaptcha/api/siteverify?secret=${getSecret("RECAPTCHA_SECRET_KEY")}&response=${recaptchaToken}`, {
+		method: "POST"
+	});
+
+	const recaptchaData = await recaptchaResponse.json();
+	console.info("reCAPTCHA data:", recaptchaData);
+
+	if (!recaptchaData.success || recaptchaData.score < 0.5) {
+		console.log("reCAPTCHA verification failed or score too low. Failing silently.", recaptchaData);
+		return false;
+	}
+
+	return true;
 }
